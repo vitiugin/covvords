60d3a716f652e8a968b59f9d4c4e6f9682aa9192
vitiugin <fv@Fedors-MBP.home>
2020-05-12 01:37:26 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+### COVID-19 Twitter Keywords Extraction
+
+For using keywords extraction for English, use the next command:
+
+`$ python3 ke_en.py USER PASSWORD SERVER "Mon May 11"`
---
+++ ke_en.py
@@ -0,0 +1,187 @@
+# code based on https://t.co/69FA0rkKUU
+
+import dns
+import spacy
+import argparse
+import json
+
+import numpy as np
+import pandas as pd
+
+from urllib.parse import quote
+from pymongo import MongoClient
+from collections import OrderedDict
+from spacy.lang.en.stop_words import STOP_WORDS
+
+STOP_WORDS.add('rt'); STOP_WORDS.add('#'); STOP_WORDS.add('%')
+STOP_WORDS.add('|')
+
+parser = argparse.ArgumentParser()
+parser.add_argument("user", help="Username for server access", type=str)
+parser.add_argument("password", help="Password for server access", type=str)
+parser.add_argument("server", help="Mongo DB server address", type=str)
+parser.add_argument("day", help='Day in twitter format. Example: "Wed May 06"', type=str)
+args = parser.parse_args()
+
+nlp = spacy.load('en_core_web_sm')
+
+class TextRank4Keyword():
+    """Extract keywords from text"""
+
+    def __init__(self):
+        self.d = 0.85 # damping coefficient, usually is .85
+        self.min_diff = 1e-5 # convergence threshold
+        self.steps = 10 # iteration steps
+        self.node_weight = None # save keywords and its weight
+
+
+    def set_stopwords(self, stopwords):
+        """Set stop words"""
+        for word in STOP_WORDS.union(set(stopwords)):
+            lexeme = nlp.vocab[word]
+            lexeme.is_stop = True
+
+    def sentence_segment(self, doc, candidate_pos, lower):
+        """Store those words only in cadidate_pos"""
+        sentences = []
+        for sent in doc.sents:
+            selected_words = []
+            for token in sent:
+                # Store words only with cadidate POS tag
+                if token.pos_ in candidate_pos and token.is_stop is False:
+                    if lower is True:
+                        selected_words.append(token.text.lower())
+                    else:
+                        selected_words.append(token.text)
+            sentences.append(selected_words)
+        return sentences
+
+    def get_vocab(self, sentences):
+        """Get all tokens"""
+        vocab = OrderedDict()
+        i = 0
+        for sentence in sentences:
+            for word in sentence:
+                if word not in vocab:
+                    vocab[word] = i
+                    i += 1
+        return vocab
+
+    def get_token_pairs(self, window_size, sentences):
+        """Build token_pairs from windows in sentences"""
+        token_pairs = list()
+        for sentence in sentences:
+            for i, word in enumerate(sentence):
+                for j in range(i+1, i+window_size):
+                    if j >= len(sentence):
+                        break
+                    pair = (word, sentence[j])
+                    if pair not in token_pairs:
+                        token_pairs.append(pair)
+        return token_pairs
+
+    def symmetrize(self, a):
+        return a + a.T - np.diag(a.diagonal())
+
+    def get_matrix(self, vocab, token_pairs):
+        """Get normalized matrix"""
+        # Build matrix
+        vocab_size = len(vocab)
+        g = np.zeros((vocab_size, vocab_size), dtype='float')
+        for word1, word2 in token_pairs:
+            i, j = vocab[word1], vocab[word2]
+            g[i][j] = 1
+
+        # Get Symmeric matrix
+        g = self.symmetrize(g)
+
+        # Normalize matrix by column
+        norm = np.sum(g, axis=0)
+        g_norm = np.divide(g, norm, where=norm!=0) # this is ignore the 0 element in norm
+
+        return g_norm
+
+
+    def get_keywords(self, number=10):
+        """Print top number keywords"""
+        node_weight = OrderedDict(sorted(self.node_weight.items(), key=lambda t: t[1], reverse=True))
+        for i, (key, value) in enumerate(node_weight.items()):
+            en_keywords.append(str(key))
+            if i > number:
+                break
+
+
+    def analyze(self, text,
+                candidate_pos=['NOUN', 'PROPN'],
+                window_size=4, lower=False, stopwords=list()):
+        """Main function to analyze text"""
+
+        # Set stop words
+        self.set_stopwords(stopwords)
+
+        # Pare text by spaCy
+        doc = nlp(text)
+
+        # Filter sentences
+        sentences = self.sentence_segment(doc, candidate_pos, lower) # list of list of words
+
+        # Build vocabulary
+        vocab = self.get_vocab(sentences)
+
+        # Get token_pairs from windows
+        token_pairs = self.get_token_pairs(window_size, sentences)
+
+        # Get normalized matrix
+        g = self.get_matrix(vocab, token_pairs)
+
+        # Initionlization for weight(pagerank value)
+        pr = np.array([1] * len(vocab))
+
+        # Iteration
+        previous_pr = 0
+        for epoch in range(self.steps):
+            pr = (1-self.d) + self.d * np.dot(g, pr)
+            if abs(previous_pr - sum(pr))  < self.min_diff:
+                break
+            else:
+                previous_pr = sum(pr)
+
+        # Get weight for each node
+        node_weight = dict()
+        for word, index in vocab.items():
+            node_weight[word] = pr[index]
+
+        self.node_weight = node_weight
+
+print('Connect to server ..')
+mongo_uri = 'mongodb+srv://' + args.user + ':' + quote(args.password) + args.server
+
+client = MongoClient(mongo_uri)
+db = client.test
+collection = db.twitters
+
+print('Done! Downloading tweets ..')
+docs = []
+cursor = collection.find({"lang": "en", "created_at": {"$regex": args.day}})
+
+docs = [document for document in cursor]
+
+print('Done! Number of tweets in dataset:', len(docs))
+
+text = ''
+for tweet in docs:
+    text += tweet['text'] + ' '
+
+print('Extracting keywords ..')
+
+tr4w = TextRank4Keyword()
+tr4w.analyze(text[:1000000], candidate_pos = ['NOUN', 'PROPN'], window_size=4, lower=False)
+
+en_keywords = []
+tr4w.get_keywords(40)
+
+export_file = {'en' :{'coronaEn': en_keywords}}
+with open('en_keywords.json', 'w') as file:
+    json.dump(export_file, file)
+
+print('Finish! Keywords file: en_keywords.json')
